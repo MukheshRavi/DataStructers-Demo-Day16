0e81185543d4406bfdc0eacfc9a1a550aeb3111f
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-23 23:03:12 +0530

    [Mukhesh].Add Insert at front in linked list

--- LinkedListclass.cs
+++ LinkedListclass.cs
@@ -39,6 +39,17 @@ namespace DataStructuresDemo
             ///next will be null for last node
             return temp;
         }
+        public void InsertFront(int new_data)
+        {
+            ///We will create a new node. The next of the new node will point to the head of the Linked list.
+            Node new_node = new Node(new_data);
+            /// When we want to add any node at the front, we want the head to point to it.
+            new_node.next = this.head;
+            ///The previous Head node is now the second node of Linked List because the new node is added at the front.
+            this.head = new_node;
+
+            Console.WriteLine("inserted into list " + new_node.data);
+        }
         /// <summary>
         /// This method displays full list
         /// </summary>
