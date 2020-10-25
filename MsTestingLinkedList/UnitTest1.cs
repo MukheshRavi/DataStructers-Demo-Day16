30391b0a8b9e59fac2f3ae5f399a79cbd07f3c6a
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-25 14:29:08 +0530

    [Mukhesh].Add Method to insert element after a specified element in linked list

--- MsTestingLinkedList/UnitTest1.cs
+++ MsTestingLinkedList/UnitTest1.cs
@@ -4,7 +4,7 @@ using DataStructuresDemo;
 namespace MsTestingLinkedList
 {
     [TestClass]
-    public class TestToSearch
+    public class TestLinkedList
     {
         LinkedListclass<int> linkedListclass;
         [TestInitialize]
@@ -15,8 +15,13 @@ namespace MsTestingLinkedList
             linkedListclass.InsertLast(30);
             linkedListclass.InsertLast(70);
         }
+        /// <summary>
+        /// This method is used to search a given element
+        /// It returns the position of element if found 
+        /// Else returns 0
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestToSearch()
         {
             ///Arrange
             int expectedPosition = 2;
@@ -25,5 +30,21 @@ namespace MsTestingLinkedList
             ///Assert
             Assert.AreEqual(expectedPosition, actualPosition);
         }
+        /// <summary>
+        /// This Test method frst adds an new element after element
+        /// And later searches for that element
+        /// </summary>
+        [TestMethod]
+        public void TestToInsertAfterElement()
+        {
+            ///Arrange
+            linkedListclass.InsertAfterElement(30, 40);
+            int expectedPosition = 3;
+            ///Act
+            int actualPosition=linkedListclass.Search(40);
+            ///Assert
+            Assert.AreEqual(expectedPosition, actualPosition);
+        } 
+
     }
 }
